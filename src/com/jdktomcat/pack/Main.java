d1adf7d46dae17c33c004e51a93ea1fc63643b5a
jdktomcat <jdktomcat@163.com>
2018-08-12 01:03:42 +0800

    线程池文档

--- src/com/jdktomcat/pack/Main.java
+++ src/com/jdktomcat/pack/Main.java
@@ -1,15 +1,12 @@
 package com.jdktomcat.pack;
 
-import java.io.File;
-
 /**
  * @author Administrator
  */
 public class Main {
 
     public static void main(String[] args) {
-        double max = Math.pow(2, 29);
-        System.out.println(max + "");
+        System.out.println(-1 << 29);
     }
 }
 
