86d35880e3feb5b534aaad54798cbdb1eae42055
汤旗 <tangqihlw@bbktel.com>
2019-05-29 16:33:21 +0800

    红黑树

---
+++ src/com/jdktomcat/pack/algorithm/redblacktree/RedBlackNode.java
@@ -0,0 +1,21 @@
+package com.jdktomcat.pack.algorithm.redblacktree;
+
+/**
+ * 类描述：红黑树节点
+ *
+ * @author 汤旗
+ * @date 2019-05-29 10:27
+ */
+public class RedBlackNode {
+
+    private int value;
+
+    private RedBlackColorEnum color;
+
+    private RedBlackNode parent;
+
+    private RedBlackNode left;
+
+    private RedBlackNode right;
+
+}
---
+++ src/com/jdktomcat/pack/algorithm/redblacktree/RedBlackTree.java
@@ -0,0 +1,10 @@
+package com.jdktomcat.pack.algorithm.redblacktree;
+
+/**
+ * 类描述：
+ *
+ * @author 汤旗
+ * @date 2019-05-29 10:28
+ */
+public class RedBlackTree {
+}
