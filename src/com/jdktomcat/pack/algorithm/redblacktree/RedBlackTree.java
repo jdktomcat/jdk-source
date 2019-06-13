0ee928965ad8218f43da971050298669e1cb13a9
汤旗 <tangqihlw@bbktel.com>
2019-06-13 20:24:45 +0800

    红黑树

--- src/com/jdktomcat/pack/algorithm/redblacktree/RedBlackTree.java
+++ src/com/jdktomcat/pack/algorithm/redblacktree/RedBlackTree.java
@@ -1,10 +1,28 @@
 package com.jdktomcat.pack.algorithm.redblacktree;
 
 /**
- * 类描述：
+ * 类描述：红黑树
  *
  * @author 汤旗
  * @date 2019-05-29 10:28
  */
 public class RedBlackTree {
+    /**
+     * 根节点
+     */
+    private RedBlackNode root;
+
+    public RedBlackTree(RedBlackNode root) {
+        this.root = root;
+    }
+
+    public RedBlackNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(RedBlackNode root) {
+        this.root = root;
+    }
+
+
 }
