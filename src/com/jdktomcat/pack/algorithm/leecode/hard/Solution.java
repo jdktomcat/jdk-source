e20018d5089b7f4e81822fd29ea6229a39b26578
汤旗 <tangqihlw@bbktel.com>
2019-06-13 20:23:10 +0800

    红黑树

---
+++ src/com/jdktomcat/pack/algorithm/leecode/hard/Solution.java
@@ -0,0 +1,18 @@
+package com.jdktomcat.pack.algorithm.leecode.hard;
+
+/**
+ * 类描述：
+ *
+ * @author 汤旗
+ * @date 2019-06-10 17:29
+ */
+public class Solution {
+
+    public static int distinctSubSequence(String s) {
+        if(s != null){
+            int length = s.length();
+
+        }
+        return 0;
+    }
+}
--- src/com/jdktomcat/pack/algorithm/redblacktree/RedBlackNode.java
+++ src/com/jdktomcat/pack/algorithm/redblacktree/RedBlackNode.java
@@ -18,4 +18,52 @@ public class RedBlackNode {
 
     private RedBlackNode right;
 
+    public RedBlackNode(int value, RedBlackColorEnum color, RedBlackNode parent,
+                        RedBlackNode left, RedBlackNode right) {
+        this.value = value;
+        this.color = color;
+        this.parent = parent;
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public RedBlackColorEnum getColor() {
+        return color;
+    }
+
+    public void setColor(RedBlackColorEnum color) {
+        this.color = color;
+    }
+
+    public RedBlackNode getParent() {
+        return parent;
+    }
+
+    public void setParent(RedBlackNode parent) {
+        this.parent = parent;
+    }
+
+    public RedBlackNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(RedBlackNode left) {
+        this.left = left;
+    }
+
+    public RedBlackNode getRight() {
+        return right;
+    }
+
+    public void setRight(RedBlackNode right) {
+        this.right = right;
+    }
 }
