964505ded0e5de18b3a256c55e3f60d61d1ed733
汤旗 <tangqihlw@bbktel.com>
2019-08-12 16:51:39 +0800

    算法题目

---
+++ src/com/jdktomcat/pack/algorithm/huawei/RandomProblem.java
@@ -0,0 +1,15 @@
+package com.jdktomcat.pack.algorithm.huawei;
+
+/**
+ * 类描述：随机数问题
+ *
+ * @author 汤旗
+ * @date 2019-08-12 16:23
+ */
+public class RandomProblem {
+
+    public static void main(String[] args) {
+
+    }
+
+}
