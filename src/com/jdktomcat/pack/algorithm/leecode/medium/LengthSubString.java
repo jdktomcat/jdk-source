1af7cabc44ceb6acb5b148c20b624263662a1a6d
汤旗 <tangqihlw@bbktel.com>
2019-08-31 14:28:45 +0800

    算法题目

---
+++ src/com/jdktomcat/pack/algorithm/leecode/medium/LengthSubString.java
@@ -0,0 +1,38 @@
+package com.jdktomcat.pack.algorithm.leecode.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 类描述：无重复字符的最长子串
+ *
+ * @author 汤旗
+ * @date 2019-08-19 20:34
+ */
+public class LengthSubString {
+
+    public static int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        if (s != null) {
+            int length = s.length();
+            for (int i = 0; i < length; i++) {
+                int num = 1;
+                for (int j = i + 1; j < length; j++) {
+                    if (s.substring(i, j).indexOf(s.charAt(j)) == -1) {
+                        num++;
+                    } else {
+                        break;
+                    }
+                }
+                max = Math.max(max, num);
+            }
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("dvdf"));
+    }
+
+}
