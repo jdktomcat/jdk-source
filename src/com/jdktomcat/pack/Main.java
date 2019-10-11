d2937f9dd47ea4b2359e10f9aaddd80e26310925
汤旗 <tangqihlw@bbktel.com>
2019-10-11 20:57:47 +0800

    字符串算法

--- src/com/jdktomcat/pack/Main.java
+++ src/com/jdktomcat/pack/Main.java
@@ -1,12 +1,17 @@
 package com.jdktomcat.pack;
 
+import java.util.Date;
+
 /**
  * @author Administrator
  */
 public class Main {
     public static void main(String[] args) {
-        String target = "%";
-        System.out.println(target.replaceAll("%|_|/",""));
+//        String target = "%";
+//        System.out.println(target.replaceAll("%|_|/",""));
+
+        Date date = new Date();
+        System.out.println(date.getTime());
 
     }
 }
