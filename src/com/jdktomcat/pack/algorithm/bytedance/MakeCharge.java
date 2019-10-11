6c5fe495fe4b64f6c6ca8810360bf7f330f1f908
汤旗 <tangqihlw@bbktel.com>
2019-10-11 21:27:07 +0800

    动态规划算法

---
+++ src/com/jdktomcat/pack/algorithm/bytedance/MakeCharge.java
@@ -0,0 +1,40 @@
+package com.jdktomcat.pack.algorithm.bytedance;
+
+/**
+ * 类描述：动态规划换零钱
+ *
+ * @author 汤旗
+ * @date 2019-10-11 21:01
+ */
+public class MakeCharge {
+
+    /**
+     * 换零钱方式数
+     *
+     * @param target 目标值
+     * @param units  单位
+     * @return 换零钱方式数
+     */
+    private static int charge(int target, int[] units) {
+        if (target == 0) {
+            return 0;
+        }
+        int best = -1;
+        for (int unit : units) {
+            int nextTry = 0;
+            if (unit <= target) {
+                nextTry = charge(target - unit, units);
+            }
+            if (best < 0 || best > nextTry + 1) {
+                best = nextTry + 1;
+            }
+        }
+        return best;
+    }
+
+    public static void main(String[] args) {
+        int[] units = new int[]{1, 2, 5, 10, 25};
+        int total = 90;
+        System.out.println(charge(total, units));
+    }
+}
