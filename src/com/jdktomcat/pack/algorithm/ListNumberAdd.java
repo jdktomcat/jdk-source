d9f7c63db56c0b4cb470cf880c6372f95dc21ba1
汤旗 <tangqihlw@bbktel.com>
2019-10-14 21:36:58 +0800

    动态规划算法

--- src/com/jdktomcat/pack/algorithm/ListNumberAdd.java
+++ src/com/jdktomcat/pack/algorithm/ListNumberAdd.java
@@ -1,6 +1,6 @@
 package com.jdktomcat.pack.algorithm;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * 类描述：
@@ -17,6 +17,7 @@ public class ListNumberAdd {
             val = x;
         }
     }
+
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode node = null;
         if (l1 != null && l2 != null) {
@@ -41,6 +42,47 @@ public class ListNumberAdd {
             node.next = addTwoNumbers(l1.next, null);
         }
         return node;
+    }
+
+    /**
+     * @param nums1
+     * @param nums2
+     * @param k
+     * @return
+     */
+    public static List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> dataList = new ArrayList<>(k);
+        TreeMap<Integer, List<List<Integer>>> treeMap = new TreeMap<>();
+        for (int number1 : nums1) {
+            for (int number2 : nums2) {
+                int sum = number1 + number2;
+                List<List<Integer>> subList = treeMap.get(sum);
+                if (subList == null) {
+                    subList = new ArrayList<>();
+                    treeMap.put(sum, subList);
+                }
+                List<Integer> subSubList = new ArrayList<>();
+                subSubList.add(number1);
+                subSubList.add(number2);
+                subList.add(subSubList);
+            }
+        }
+        int size = 0;
+        Iterator<Integer> iterator = treeMap.keySet().iterator();
+        while (size <= k && iterator.hasNext()) {
+            List<List<Integer>> subList = treeMap.get(iterator.next());
+            int sub = size < k ? subList.size() : k - size;
+            dataList.addAll(subList.subList(0, sub));
+            size += subList.size();
+        }
+        return dataList.subList(0, k);
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 7, 11};
+        int[] nums2 = {2, 4, 6};
+        int k = 3;
+        List<List<Integer>> dataList = kSmallestPairs(nums1, nums2, k);
     }
 }
 
