8ac3964873fab31a5c5db13ea632eed1d037829b
汤旗 <tangqihlw@bbktel.com>
2019-10-14 10:07:56 +0800

    动态规划算法

--- src/com/jdktomcat/pack/algorithm/bytedance/MakeCharge.java
+++ src/com/jdktomcat/pack/algorithm/bytedance/MakeCharge.java
@@ -1,5 +1,7 @@
 package com.jdktomcat.pack.algorithm.bytedance;
 
+import java.util.Arrays;
+
 /**
  * 类描述：动态规划换零钱
  *
@@ -12,8 +14,6 @@ public class MakeCharge {
      * 最大面额
      */
     private static int max_unit = 0;
-
-    private static int nextTry = 0;
 
     /**
      * 换零钱方式数最少钱数
@@ -29,41 +29,13 @@ public class MakeCharge {
         int best = -1;
         for (int unit : units) {
             if (unit <= target) {
-                nextTry = minUnit(target - unit, units);
-            }
-            if (best < 0 || best > nextTry + 1) {
-                best = nextTry + 1;
+                int nextTry = minUnit(target - unit, units);
+                if (nextTry >= 0 && (best < 0 || best > nextTry + 1)) {
+                    best = nextTry + 1;
+                }
             }
         }
         return best;
-    }
-
-    /**
-     * 换钱方式总数
-     *
-     * @param target 目标钱数
-     * @param units  钱单位
-     * @return 方式总数
-     */
-    private static int total(int target, int[] units, int unit) {
-        if (target == 0) {
-            if (unit >= max_unit) {
-                max_unit = unit;
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-        int total = 0;
-        for (int in = 0; in < units.length; in++) {
-            int unitIn = units[in];
-            if (unitIn <= target) {
-                total += total(target - unitIn, units, unitIn);
-            } else {
-                break;
-            }
-        }
-        return total;
     }
 
     /**
@@ -71,22 +43,18 @@ public class MakeCharge {
      *
      * @param target 目标
      * @param units  面值数组
-     * @param unitIn last unit
      * @return 方式
      */
-    public static int charge(int target, int[] units, int unitIn) {
+    public static int charge(int target, int[] units, int startIndex) {
         if (target == 0) {
-            if (unitIn > max_unit) {
-                max_unit = unitIn;
-                return 1;
-            } else {
-                return 0;
-            }
+            return 1;
         }
         int total = 0;
-        for (int unit : units) {
+
+        for (int index = startIndex; index < units.length; index++) {
+            int unit = units[index];
             if (unit <= target) {
-                total += charge(target - unit, units, unit);
+                total += charge(target - unit, units, index);
             } else {
                 break;
             }
@@ -94,9 +62,13 @@ public class MakeCharge {
         return total;
     }
 
+    public static int change(int amount, int[] coins) {
+        return charge(amount, coins, 0);
+    }
+
     public static void main(String[] args) {
-        int[] units = new int[]{1, 2, 5, 25};
-        int total = 24;
-        System.out.println(String.format("target:%d  total:%d", total, minUnit(total, units)));
+        int[] units = {1, 99};
+        int total = 100;
+        System.out.println(String.format("target:%d  total:%d", total, change(total, units)));
     }
 }
