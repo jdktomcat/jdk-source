b76f685ed386a1daee2bd9a6807a96010e223045
家笔记本 <jdktomcat@163.com>
2020-11-19 01:10:45 +0800

    设计模式

--- src/com/jdktomcat/pack/pattern/builder/ComputerConcreteBuilder.java
+++ src/com/jdktomcat/pack/pattern/builder/ComputerConcreteBuilder.java
@@ -6,23 +6,32 @@ public class ComputerConcreteBuilder implements ComputerBuilder {
 
     Computer computer;
 
+    private static Logger logger = Logger.getLogger(ComputerConcreteBuilder.class.getName());
+
+    public ComputerConcreteBuilder() {
+        this.computer = new Computer();
+    }
+
     @Override
     public void buildCpu() {
-
+        logger.info("build cpu.....");
+        computer.setCpu("8core");
     }
 
     @Override
     public void buildMemory() {
-
+        logger.info("build memory.....");
+        computer.setMemory("8g");
     }
 
     @Override
     public void buildDisk() {
-
+        logger.info("build disk.....");
+        computer.setDisk("1t");
     }
 
     @Override
     public Computer buildComputer() {
-        return null;
+        return computer;
     }
 }
