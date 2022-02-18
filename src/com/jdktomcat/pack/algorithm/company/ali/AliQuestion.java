4aa6bfa326a04aa91bd7edd430671f42c9696a02
tangqi <tangqi.tang@alibaba-inc.com>
2022-02-18 10:30:56 +0800

    优化

--- src/com/jdktomcat/pack/algorithm/company/ali/AliQuestion.java
+++ src/com/jdktomcat/pack/algorithm/company/ali/AliQuestion.java
@@ -3,32 +3,23 @@ package com.jdktomcat.pack.algorithm.company.ali;
 public class AliQuestion {
 
     public static int minSumSubArray(int[] dataArray, int start, int end) {
-        int min = 0;
-        if (start < end) {
-            for (int index = start; index < end; index++) {
-                min += dataArray[index];
-            }
-            int head = dataArray[start];
-            int tail = dataArray[end - 1];
-            int subMin;
-            if (head > tail) {
-                min -= dataArray[start];
-                subMin = minSumSubArray(dataArray, start + 1, end);
-            } else if (head < tail) {
-                min -= dataArray[end -1];
-                subMin = minSumSubArray(dataArray, start, end - 1);
+        //
+        int preEndMinSum = 0;
+        int ans = 0;
+        for (int i=start; i< end; i++) {
+            if (preEndMinSum < 0) {
+                preEndMinSum += dataArray[i];
             } else {
-                min -= dataArray[start];
-                min -= dataArray[end - 1];
-                subMin = minSumSubArray(dataArray, start + 1, end - 1);
+                preEndMinSum = dataArray[i];
             }
-            min = Math.min(subMin, min);
+            ans = Math.min(ans, preEndMinSum);
+
         }
-        return min;
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[] dataArray = new int[]{1, -2, 3, 10, -4, -7, 2, -5};
+        int[] dataArray = new int[]{1, -2, 3, -10, 4, 7, 2, 5};
         System.out.println(minSumSubArray(dataArray, 0, dataArray.length));
     }
 }
