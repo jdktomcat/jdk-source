a699d7c65bc97bc65a91e3d7c4c1bc34e97bb69f
tangqi <tangqi.tang@alibaba-inc.com>
2022-05-27 09:50:09 +0800

    练习

--- src/com/jdktomcat/pack/file/FileScanner.java
+++ src/com/jdktomcat/pack/file/FileScanner.java
@@ -1,9 +1,7 @@
 package com.jdktomcat.pack.file;
 
 import java.io.*;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 类描述：文件扫描
@@ -12,27 +10,28 @@ import java.util.Set;
  * @date 2019-11-05 16:26
  */
 public class FileScanner {
-    private static Set<String> getFileSet(String filePath) throws FileNotFoundException {
-        Set<String> imeiSet = new HashSet<>();
+    private static Map<String, String> getFileSet(String filePath) throws FileNotFoundException {
+        Map<String, String> dataMap = new HashMap<>();
         Scanner scanner = new Scanner(new File(filePath));
         while (scanner.hasNext()) {
-            imeiSet.add(scanner.next().trim());
+            String line = scanner.next().trim();
+            String[] params = line.split(",");
+            if (params.length == 2) {
+                dataMap.put(params[0].trim(), params[1].trim());
+            }
         }
-        return imeiSet;
+        return dataMap;
     }
 
     public static void main(String[] args) throws IOException {
-        String myDataFile = "/Users/tangqi/Desktop/shop.txt";
-        String theirDataFile = "/Users/tangqi/Desktop/xiniu.txt";
-        Set<String> myData = getFileSet(myDataFile);
-        Set<String> theirData = getFileSet(theirDataFile);
-        Set<String> result = new HashSet<>();
-        result.clear();
-        result.addAll(myData);
-        result.removeAll(theirData);
-        System.out.println("数据总数差异（条数：" + result.size() + "）：");
-        for (String imei : result) {
-            System.out.println(imei);
-        }
+        String sourceFile = "/Users/tangqi/Desktop/source.txt";
+        String theirDataFile = "/Users/tangqi/Desktop/curl.txt";
+        String cmd = "curl --location --request POST 'http://127.0.0.1:7001/mp/shop/system/sendMetaProducerMessage' \\\n" + "--header 'Content-Type: application/x-www-form-urlencoded' \\\n" + "--data-urlencode 'topic=AMAP-MP-SHOP-CREATE-TOPIC' \\\n" + "--data-urlencode 'tag=' \\\n" + "--data-urlencode 'key=' \\\n" + "--data-urlencode 'body={\"merchantId\":\"1088013104187694\",\"cpid\":\"%s\",\"source\":\"wuliangye_api\",\"shopId\":\"%s\"}'";
+        Map<String, String> dataMap = getFileSet(sourceFile);
+        PrintWriter printWriter = new PrintWriter(theirDataFile);
+        dataMap.forEach((key, value) -> {
+            printWriter.println(String.format(cmd, key, value));
+            printWriter.flush();
+        });
     }
 }
